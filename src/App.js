19c8f3de875646a003481033c49946b457d82323
pavan <pavan.bnmk@gmail.com>
2024-04-28 14:37:08 +0530

    About component added and new feature added in text form utility

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './About';
 
 function App() {
   return (
     <>
       <Navbar title="Text Utils" aboutText="About us" />
       <TextForm heading="Enter the text to analyze below: "/>
+      {/* <About /> */}
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 export default function TextForm(props) {
 
     const [text, setText] = useState("");
+    const [copyText, setCopyText] = useState("Copy");
 
     //text = "new text";// wrong way to change the state. 
     //setText("new text");// correct way to change the state.
@@ -17,6 +18,25 @@ export default function TextForm(props) {
         setText(newText);
     }
 
+    const handleClearText = () => {
+        let newText = "";
+        setText(newText);
+    }
+
+    const handleCopy = () => {
+        let text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+        setCopyText("Copied");
+        setTimeout(()=>setCopyText('Copy'), 5000);
+    }
+
+    const handleRemoveSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText( newText.join(" ") );
+    }
+    
+
     const handleOnChange = (e) => {
         setText(e.target.value);
     }
@@ -30,12 +50,18 @@ export default function TextForm(props) {
                     <textarea className='form-control' id="myBox" value={text} rows={6} onChange={handleOnChange} placeholder='Enter your text'></textarea>
                     <button className='btn btn-primary my-2' onClick={handleUpClick}>Convert to uppercase</button>
                     <button className='btn btn-primary my-2 mx-2' onClick={handleLowerClick}>Convert to lowercase</button>
-                    {/* <button className='btn btn-primary my-2' onClick={handleUpClick}>Convert to uppercase</button> */}
+                    <button className='btn btn-primary my-2 mx-2' onClick={handleClearText}>Clear</button>
+                    <button className='btn btn-primary my-2 mx-2' onClick={handleCopy}>{copyText}</button>
+                    <button className='btn btn-primary my-2' onClick={handleRemoveSpaces}>Remove extra spaces</button>
+                    
                 </div>
             </div>
             <div className='container my-3'>
                 <h6>Your Text Summary</h6>
                 <p>{text.split(" ").length} words and {text.length} characters</p>
+                <p>{0.008 * text.split(" ").length} Minutes read</p>
+                <h6>Preview</h6>
+                <p>{text}</p>
             </div>
         </>
     )
